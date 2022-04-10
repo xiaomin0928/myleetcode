c6b8f5306c73d0cf643078ed9400e7fa0bb660c1
apple <wangxm0928@gmail.com>
2022-04-10 13:18:02 +0200

    SearchInsertPosition

---
+++ src/leetcode/editor/cn/SearchInsertPosition.java
@@ -0,0 +1,66 @@
+//Given a sorted array of distinct integers and a target value, return the 
+//index if the target is found. If not, return the index where it would be if it were 
+//inserted in order. 
+//
+// You must write an algorithm with O(log n) runtime complexity. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: nums = [1,3,5,6], target = 5
+//Output: 2
+// 
+//
+// Example 2: 
+//
+// 
+//Input: nums = [1,3,5,6], target = 2
+//Output: 1
+// 
+//
+// Example 3: 
+//
+// 
+//Input: nums = [1,3,5,6], target = 7
+//Output: 4
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= nums.length <= 10⁴ 
+// -10⁴ <= nums[i] <= 10⁴ 
+// nums contains distinct values sorted in ascending order. 
+// -10⁴ <= target <= 10⁴ 
+// 
+// Related Topics 数组 二分查找 👍 1478 👎 0
+
+
+package leetcode.editor.cn;
+class SearchInsertPosition{
+    public static void main(String[] args) {
+        Solution solution = new SearchInsertPosition().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1, middle;
+        while (left <= right) {
+            middle = left + ((right - left) >> 1);
+            if (nums[middle] > target) {
+                right = middle - 1;
+            } else if (nums[middle] < target) {
+                left = middle + 1;
+            } else {
+                return middle;
+            }
+        }
+        return left;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
